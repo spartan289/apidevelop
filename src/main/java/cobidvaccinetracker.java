d0dbd321d0a407bd2d6035e38236e3c5725ed289
spartan289 <60476934+spartan289@users.noreply.github.com>
2021-07-06 20:22:13 +0530

    json data extract

--- src/main/java/cobidvaccinetracker.java
+++ src/main/java/cobidvaccinetracker.java
@@ -1,38 +1,69 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
+import java.util.Map;
 import java.util.Scanner;
 public class cobidvaccinetracker {
     public static void main(String[] args) throws Exception{
     Scanner sc = new Scanner(System.in);
-    String date = "02-07-2021";
+    String date = "06-07-2021";
     String pincode = sc.nextLine();
+    try {
 
-    String host = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+pincode+"&date="+date;
-    System.out.println(host);
-    HttpResponse <JsonNode> response = Unirest.get(host)
-            .asJson();
+        String host = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pincode + "&date=" + date;
+        System.out.println(host);
+        HttpResponse<JsonNode> response = Unirest.get(host)
+                .asJson();
 
-    System.out.println(response.getStatus());
-    System.out.println(response.getBody());
-    //below part is to show the json file in readable format
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    JsonParser jp = new JsonParser();
-    JsonElement je = jp.parse(response.getBody().toString());
-    String prettyJsonString = gson.toJson(je);
-    System.out.println(prettyJsonString);
-    Object obj = new JSONParser().parse(String.valueOf(response.getBody()));
-    JSONObject jo = (JSONObject) obj;
+        System.out.println(response.getStatus());
+        System.out.println(response.getBody());
+        //below part is to show the json file in readable format
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(response.getBody().toString());
+        String prettyJsonString = gson.toJson(je);
+        System.out.println(prettyJsonString);
+        String readLine = null;
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(response.getRawBody()));
+        StringBuilder responses = new StringBuilder();
+        while ((readLine = in.readLine()) != null) {
+            responses.append(readLine);
+        }//print result
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(responses.toString());
+        JSONArray responseArr = (JSONArray) json.get("sessions");
+        for (Object object : responseArr) {
+            JSONObject jsonObj = (JSONObject) object;
+            Long pincodel = (Long) jsonObj.get("pincode");
+            String name = (String) jsonObj.get("name");
+            String address = (String) jsonObj.get("address") + ", " + (String) jsonObj.get("district_name") + ", " +
+                    (String) jsonObj.get("state_name") + " " + pincodel;
+            String fee_type = (String) jsonObj.get("fee_type");
+            System.out.println(pincodel);
+            System.out.println(name);
+            System.out.println(address);
+            System.out.println(fee_type);
+        }
+
+    } catch (UnirestException | JsonSyntaxException | IOException | ParseException e) {
+        e.printStackTrace();
+    }
 
     }
+
+
 
 }
